852993e27c7ec5b8ed43e4d48c23cdcb192165cb
gfalexander <alexander.ferreira@sptech.school>
2023-08-07 13:56:17 -0300

    Logging Adding log to gunicorn and flask Sort imports

--- billing/service/auth_service.py
+++ billing/service/auth_service.py
@@ -1,17 +1,15 @@
-from billing.models.user.dto import UserMapper
 from billing.models.user.data import UserRepository
+from billing.models.user.dto import UserMapper
+
 
 class AuthService:
-
     def register(self, request_json: dict):
         user = UserMapper.toCreateDto(request_json)
 
         created_user = UserRepository().save(user)
-        breakpoint()
 
         if created_user is None:
             # TODO: Create a custom exception
             raise Exception("User already exists")
         else:
             return UserMapper.toDto(created_user)
-        
--- billing/utils/exceptions/__init__.py
+++ billing/utils/exceptions/__init__.py
@@ -1,7 +1,6 @@
 from .invalid_type_exception import InvalidTypeException
 from .missing_key_exception import MissingKeyException
 
-
 __all__ = [
     "InvalidTypeException",
     "MissingKeyException",
